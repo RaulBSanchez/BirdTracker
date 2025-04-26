69b07453a7744d21ad8ac917c742e711355d0d5f
Raul Bazan <raulbazan@mac.mynetworksettings.com>
2025-04-26 15:35:23 -0400

    added a function to iterate over a time period

--- historic.py
+++ historic.py
@@ -2,10 +2,18 @@ import requests
 import json
 from dotenv import load_dotenv, dotenv_values 
 import os
+
+import datetime
+from time import sleep
 load_dotenv()
 
+START_DATE = datetime.date(2025, 4, 23)
+END_DATE = datetime.date(2025, 4, 26)
+
+
+
 # URL for hotspot data
-url = 'https://api.ebird.org/v2/data/obs/L3041917/historic/2025/04/17'
+#url = 'https://api.ebird.org/v2/data/obs/L3041917/historic/2025/04/17'
 client_api = os.getenv('API_KEY')
 
 
@@ -15,21 +23,29 @@ headers = {
 }
 
 # Send GET request to fetch hotspots
-response = requests.get(url, headers=headers)
+#response = requests.get(url, headers=headers)
 
-
+current_date = START_DATE
 # url = "https://api.ebird.org/v2/data/obs/{{regionCode}}/historic/{{y}}/{{m}}/{{d}}"
 
-if response.status_code == 200:
-    # Get the raw response text
-    response_text = response.text
-    data = json.loads(response_text)
-    print(type(data))
+for i in range((END_DATE - START_DATE).days + 1):
+    current_date = START_DATE + datetime.timedelta(days=i)
+    print(current_date)
+    url = f'https://api.ebird.org/v2/data/obs/L3041917/historic/{current_date.year}/{current_date.month}/{current_date.day}'
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        # Get the raw response text
+        response_text = response.text
+        data = json.loads(response_text)
+        #print(type(data))
     
-    for x in data:
+        for x in data:
 
-        #print("common name:",  x["comName"])
-        #print("Count:", x["howMany"])
-        print(x)
-else:
-    print("no good")
+            print("common name:",  x["comName"])
+            print("Count:", x["howMany"])
+            
+    else:
+        print("no good")
+    #current_date += datetime.timedelta(days=1)
+
+
