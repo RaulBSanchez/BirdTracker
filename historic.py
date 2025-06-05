020833f97ac6721c86971bcb8f10ad8a28422b4d
Raul Bazan <raulbazan@mac.mynetworksettings.com>
2025-06-05 08:17:26 -0400

    able to get all in info by month, year

--- historic.py
+++ historic.py
@@ -8,22 +8,26 @@ from time import sleep
 load_dotenv()
 import time
 from pathlib import Path
+import calendar
 
-START_DATE = datetime.date(2025, 1, 1)
-END_DATE = datetime.date(2025, 1, 31)
 
 
 print("Enter Year")
-year = input()
-print(year)
-print("Enter Month")
-month = input()
-print(month)
+year = int(input())
+string_year = str(year)
+print("Enter Month as a number")
+month = int(input())
 
-filepath = Path('/Users/raulbazan/Desktop/HistoricalData/DixonMeadowPreserve/2025/January2025.csv')
+month_name = calendar.month_name[month]
+print(month_name + " this is the month name")
 
-# URL for hotspot data
-#url = 'https://api.ebird.org/v2/data/obs/L3041917/historic/2025/04/17'
+csv_file_name = month_name + string_year
+path = '/Users/raulbazan/Desktop/HistoricalData/DixonMeadowPreserve/2025/' + '/' + csv_file_name + '.csv'
+filepath = Path(path)
+
+
+
+
 client_api = os.getenv('API_KEY')
 # L3041917
 
@@ -32,48 +36,41 @@ headers = {
     'X-eBirdApiToken': client_api
 }
 
-# Send GET request to fetch hotspots
-#response = requests.get(url, headers=headers)
+
 
 bird_count = {}
 
-current_date = START_DATE
-# url = "https://api.ebird.org/v2/data/obs/{{regionCode}}/historic/{{y}}/{{m}}/{{d}}"
+
 df = pd.DataFrame()
-data = {'Name': ['Alice', 'Bob', 'Charlie'],
-        'Age': [25, 30, 35],
-        'City': ['New York', 'London', 'Paris']}
-df2 = pd.DataFrame(data)
 
-# for i in range((END_DATE - START_DATE).days + 1):
-#     current_date = START_DATE + datetime.timedelta(days=i)
-#     #print(current_date)
-#     url = f'https://api.ebird.org/v2/data/obs/L3041917/historic/{current_date.year}/{current_date.month}/{current_date.day}'
-#     response = requests.get(url, headers=headers)
-#     if response.status_code == 200:
-#         # Get the raw response text
-#         response_text = response.text
-#         data = json.loads(response_text)
-#         #print(data)
-#         if data:
-#             day_df = pd.DataFrame(data)
-#             df = pd.concat([df, day_df], ignore_index=True)
-#         else:
-#             print(" no data for this date")
-#     else:
-#         print("better luck next time")
+num_days = calendar.monthrange(year, month)[1]
+print(num_days)
+
+df2 = pd.DataFrame()
+
+for i in range(1, num_days + 1):
+    #current_date = START_DATE + datetime.timedelta(days=i)
+    #print(current_date)
+    url = f'https://api.ebird.org/v2/data/obs/L3041917/historic/{year}/{month}/{i}'
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        # Get the raw response text
+        response_text = response.text
+        data = json.loads(response_text)
+        #print(data)
+        if data:
+            day_df = pd.DataFrame(data)
+            df = pd.concat([df, day_df], ignore_index=True)
+        else:
+            print(" no data for this date" , " ", month , " " , i)
+    else:
+        print("better luck next time")
     
-#     time.sleep(1)
+    time.sleep(1)
 
 
-            
-   
-#     #current_date += datetime.timedelta(days=1)
 
-# #df.to_csv('your_file_name2.csv', index=False)
+if not df.empty and 'obsDt' in df.columns:
+    df['obsDt'] = pd.to_datetime(df['obsDt'], errors='coerce')
 
-# if not df2.empty and 'obsDt' in df.columns:
-#     df['obsDt'] = pd.to_datetime(df['obsDt'], errors='coerce')
-
-# df2.to_csv(filepath, index=False)
-# print("Data saved to ebird_dixon_observations.csv")
+df.to_csv(filepath, index=False)
