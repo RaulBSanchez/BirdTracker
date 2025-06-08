ad63251e0e134719cc74362bccc20741029fbb1b
Raul Bazan <Raul.BazanSanchez@gmail.com>
2025-06-08 10:53:55 -0400

    cleaning up data

--- cleanup.py
+++ cleanup.py
@@ -8,3 +8,11 @@ print(df)
 
 
 print(df.dtypes)
+
+git config user.email "Raul.BazanSanchez@gmail.com"
+
+2025-04-30 08:33:00
+
+df['Mycol'] = pd.to_datetime(df['Mycol'], format='%Y%m%d:%H:%M:%S')
+
+'%Y%d%d:%H:%M:%S'
