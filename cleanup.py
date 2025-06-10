354f729fbfe68d65ebc8c08df58826e8a5977224
Raul Bazan <Raul.BazanSanchez@gmail.com>
2025-06-10 09:46:28 -0400

    moving the clean data out to its directory

--- cleanup.py
+++ cleanup.py
@@ -2,17 +2,6 @@ import pandas as pd
 import os
 from pathlib import Path
 
-
-# csv_files = list(Path("/Users/raulbazan/Desktop/HistoricalData/DixonMeadowPreserve/2025").glob("*.csv"))
-
-# for x in csv_files:
-# 	print(x)
-
-
-
-from pathlib import Path
-import pandas as pd
-
 def getCsvFiles(path):
     try:
         directory = Path(path)
@@ -29,18 +18,30 @@ def getCsvFiles(path):
 
 def dataFrameCreator(file, directory):
 	df = pd.read_csv(file)
+	string_path = str(file)
+	csv_name = string_path.split("/")[-1]
+	
+
+	
 	df['birdCount'] = df['howMany'].astype('Int64')
 	df = df.drop(['obsValid', 'obsReviewed', 'locationPrivate', 'exoticCategory', 'subId', 'howMany'], axis=1)
 	df['obsDt'] = pd.to_datetime(df['obsDt'], format='%Y-%m-%d %H:%M:%S') 
 	df = df.dropna(axis=0, subset=['birdCount'])
 	df = df.reset_index(drop=True)
-	print(df)
-
 	
-
+	target_path = '/Users/raulbazan/Desktop/CleanData/DixonMeadowPreserve/2025/' + csv_name
+	cleandatapath = Path(target_path)
+	#print(target_path)
+	try: 
+		df.to_csv(cleandatapath, index=False)
+	
+	except:
+		print("didnt work")
+		#print(target_path)
 
 
 directory_path = "/Users/raulbazan/Desktop/HistoricalData/DixonMeadowPreserve/2025/"
+target_path = "/Users/raulbazan/Desktop/CleanData/DixonMeadowPreserve/2025"
 dfs = getCsvFiles(directory_path)
 
 # Example: view one
