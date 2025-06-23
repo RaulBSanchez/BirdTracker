bd602208184dd920b189402da64bb50d006b7dc6
Raul Bazan <Raul.BazanSanchez@gmail.com>
2025-06-23 15:46:17 -0400

    cleaning up code

--- historic.py
+++ historic.py
@@ -15,11 +15,11 @@ import calendar
 print("Enter Year")
 year = int(input())
 string_year = str(year)
-print("Enter Month as a number")
-month = int(input())
+# print("Enter Month as a number")
+# month = int(input())
 
-month_name = calendar.month_name[month]
-print(month_name + " this is the month name")
+# month_name = calendar.month_name[month]
+# print(month_name + " this is the month name")
 
 csv_file_name = month_name + string_year
 path = '/Users/raulbazan/Desktop/HistoricalData/FDRPark/2021/' + '/' + csv_file_name + '.csv'
@@ -46,31 +46,33 @@ df = pd.DataFrame()
 num_days = calendar.monthrange(year, month)[1]
 print(num_days)
 
-df2 = pd.DataFrame()
+# df2 = pd.DataFrame()
 
-for i in range(1, num_days + 1):
-    #current_date = START_DATE + datetime.timedelta(days=i)
-    #print(current_date)
-    url = f'https://api.ebird.org/v2/data/obs/L1025768/historic/{year}/{month}/{i}'
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        # Get the raw response text
-        response_text = response.text
-        data = json.loads(response_text)
-        #print(data)
-        if data:
-            day_df = pd.DataFrame(data)
-            df = pd.concat([df, day_df], ignore_index=True)
+
+def dataFetcher():
+    for i in range(1, num_days + 1):
+        #current_date = START_DATE + datetime.timedelta(days=i)
+        #print(current_date)
+        url = f'https://api.ebird.org/v2/data/obs/L1025768/historic/{year}/{month}/{i}'
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            # Get the raw response text
+            response_text = response.text
+            data = json.loads(response_text)
+            #print(data)
+            if data:
+                day_df = pd.DataFrame(data)
+                df = pd.concat([df, day_df], ignore_index=True)
+            else:
+                print(" no data for this date" , " ", month , " " , i)
         else:
-            print(" no data for this date" , " ", month , " " , i)
-    else:
-        print("better luck next time")
-    
-    time.sleep(1)
+            print("better luck next time")
+        
+        time.sleep(1)
 
 
 
-if not df.empty and 'obsDt' in df.columns:
-    df['obsDt'] = pd.to_datetime(df['obsDt'], errors='coerce')
+    if not df.empty and 'obsDt' in df.columns:
+        df['obsDt'] = pd.to_datetime(df['obsDt'], errors='coerce')
 
-df.to_csv(filepath, index=False)
+    df.to_csv(filepath, index=False)
