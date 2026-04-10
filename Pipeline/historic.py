e98a97469c63b66f328bb9a99ee3faa6a7452bf2
Raul Bazan <Raul.BazanSanchez@gmail.com>
2026-04-10 14:47:26 -0400

    making sure I dont try to fetch data for future dates

--- Pipeline/historic.py
+++ Pipeline/historic.py
@@ -25,57 +25,50 @@ def dataFetcher(month_name, num_days, year, month):
     #year = year
     df = pd.DataFrame()
 
-    x = '1'
-
-    #s = string_year + '/' + string_month + '/' + x 
-    #print(f"{s}, this is s")
+    
 
     client_api = os.getenv('API_KEY')
+    current_date = datetime.date(year, month, 1)
+    today = datetime.date.today()
 
+
+    print(f"{today}, todays date, current date {current_date}")
 
 # Set the API key in the request headers
     headers = {
         'X-eBirdApiToken': client_api
     }
     #print(headers, client_api)
-
-    for i in range(1, num_days + 1):
-        current_date = datetime.date(year, month, i)
-        today = datetime.date.today()
-
-        while current_date < today:
-            print(today, "todays data")
-            print(current_date, "this is current date for loop")
-            url = f"https://api.ebird.org/v2/data/obs/L1069194/historic/{year}/{month}/{i}"
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                # Get the raw response text
-                response_text = response.text
-                data = json.loads(response_text)
-                #print(data)
-                if data:
-                    day_df = pd.DataFrame(data)
-                    df = pd.concat([df, day_df], ignore_index=True)
-                else:
-                    print(f"No data available for {i}, {month}, {year}")
-            else:
-                print("Failed to get a response from eBird")
+    # while current_date < today:
+    #     for i in range(1, num_days + 1):
             
-            time.sleep(1)
+    #         print(today, "todays data")
+    #         print(current_date, "this is current date for loop")
+    #         url = f"https://api.ebird.org/v2/data/obs/L1069194/historic/{year}/{month}/{i}"
+    #         response = requests.get(url, headers=headers)
+    #         if response.status_code == 200:
+    #                 # Get the raw response text
+    #             response_text = response.text
+    #             data = json.loads(response_text)
+    #                 #print(data)
+    #             if data:
+    #                 day_df = pd.DataFrame(data)
+    #                 df = pd.concat([df, day_df], ignore_index=True)
+    #             else:
+    #                 print(f"No data available for {i}, {month}, {year}")
+    #         else:
+    #             print("Failed to get a response from eBird")
+                
+    #         time.sleep(1)
 
-            current_date += datetime.timedelta(days=1)
+    #         current_date += datetime.timedelta(days=1)
 
 
+    #     if not df.empty and 'obsDt' in df.columns:
+    #         df['obsDt'] = pd.to_datetime(df['obsDt'], errors='coerce')
 
-
-
-
-
-    if not df.empty and 'obsDt' in df.columns:
-        df['obsDt'] = pd.to_datetime(df['obsDt'], errors='coerce')
-
-            # #df.to_csv(filepath, index=False)
-        print(df)
+    #             # #df.to_csv(filepath, index=False)
+    #         print(df)
 
 def main():
     # This method will iterate over the months and call the data fetcher in order to 
@@ -129,9 +122,16 @@ def main():
             
 
 
-    for month in range(4, 13):
+    for month in range(1, 13):
         month_name = calendar.month_name[month]
         num_days = calendar.monthrange(year, month)[1]
+
+        today = datetime.date.today()
+        print(today.year, today.month)
+        print(today, " todays month comparison")
+        if year == today.year and month > today.month:
+            break
+        
         dataFetcher(month_name, num_days, year, month)
     
 
@@ -141,8 +141,6 @@ def testHistory():
 
 if __name__ == "__main__":
     main()
-    now = datetime.date.today()
-
-    print(now)
+  
 
 
