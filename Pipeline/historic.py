dfb0f8d518f09cbd5501cae5f20aa509d81d848e
Raul Bazan <Raul.BazanSanchez@gmail.com>
2026-04-16 13:49:55 -0400

    finished up the script, will clean it up later

--- Pipeline/historic.py
+++ Pipeline/historic.py
@@ -12,63 +12,75 @@ import calendar
 #from datetime import date
 
 
-def dataFetcher(month_name, num_days, year, month):
+def dataFetcher(month_name, num_days, year, month, locationTag):
     print("this is from data fetcher")
     #print(month_name, num_days, year, month)
+
 
     string_year = str(year)
     string_month = str(month)
     csv_file_name = 'NavalYard' + month_name + string_year
-    path = '/Users/raulbazan/Desktop/HistoricalData/NewData/' + '/' + csv_file_name + '.csv'
+    path = '/Users/raulbazan/Projects/BirdTracker/Data' + '/' + csv_file_name + '.csv'
     filepath = Path(path)
     #month = month_name
     #year = year
     df = pd.DataFrame()
 
+    print(locationTag, "from data fetcher")
     
 
     client_api = os.getenv('API_KEY')
-    current_date = datetime.date(year, month, 1)
+    
     today = datetime.date.today()
 
 
-    print(f"{today}, todays date, current date {current_date}")
+    #print(f"{today}, todays date, current date {current_date}")
 
 # Set the API key in the request headers
     headers = {
         'X-eBirdApiToken': client_api
     }
     #print(headers, client_api)
-    # while current_date < today:
-    #     for i in range(1, num_days + 1):
-            
-    #         print(today, "todays data")
-    #         print(current_date, "this is current date for loop")
-    #         url = f"https://api.ebird.org/v2/data/obs/L1069194/historic/{year}/{month}/{i}"
-    #         response = requests.get(url, headers=headers)
-    #         if response.status_code == 200:
-    #                 # Get the raw response text
-    #             response_text = response.text
-    #             data = json.loads(response_text)
-    #                 #print(data)
-    #             if data:
-    #                 day_df = pd.DataFrame(data)
-    #                 df = pd.concat([df, day_df], ignore_index=True)
-    #             else:
-    #                 print(f"No data available for {i}, {month}, {year}")
-    #         else:
-    #             print("Failed to get a response from eBird")
+    for i in range(1, num_days + 1):
+        print(i)
+        # print("this is from the looooop")
+        current_date = datetime.date(year, month, i)
+        print(f"{current_date}, current date, {today}, today")
+        
+        if current_date >= today:
+            break
+
+        
+        
+        print(today, "todays data")
+        print(current_date, "this is current date for loop")
+        url = f"https://api.ebird.org/v2/data/obs/{locationTag}/historic/{year}/{month}/{i}"
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+                    # Get the raw response text
+            response_text = response.text
+            data = json.loads(response_text)
+                    #print(data)
+            if data:
+                day_df = pd.DataFrame(data)
+                df = pd.concat([df, day_df], ignore_index=True)
+            else:
+                print(f"No data available for {i}, {month}, {year}")
+        else:
+            print("Failed to get a response from eBird")
                 
-    #         time.sleep(1)
+        time.sleep(1)
 
-    #         current_date += datetime.timedelta(days=1)
+        current_date += datetime.timedelta(days=1)
 
 
-    #     if not df.empty and 'obsDt' in df.columns:
-    #         df['obsDt'] = pd.to_datetime(df['obsDt'], errors='coerce')
+        if not df.empty and 'obsDt' in df.columns:
+            df['obsDt'] = pd.to_datetime(df['obsDt'], errors='coerce')
 
-    #             # #df.to_csv(filepath, index=False)
-    #         print(df)
+                # #df.to_csv(filepath, index=False)
+
+    df.to_csv(filepath, index=False)
+
 
 def main():
     # This method will iterate over the months and call the data fetcher in order to 
@@ -94,6 +106,8 @@ def main():
     while True:    
         try:
             locationValue = int(input("Enter number: "))
+            locationTag = locations[locationValue][0]
+            #print(locationTag, "from selector")
             if locationValue not in locations:
                 print("Please enter valid choice")
                 continue
@@ -120,24 +134,18 @@ def main():
             print("Enter A valid choice")
 
             
-
-
-    for month in range(1, 13):
+    for month in range(3, 13):
         month_name = calendar.month_name[month]
         num_days = calendar.monthrange(year, month)[1]
 
         today = datetime.date.today()
-        print(today.year, today.month)
-        print(today, " todays month comparison")
+        #print(today.year, today.month)
+        #print(today, " todays month comparison")
         if year == today.year and month > today.month:
             break
         
-        dataFetcher(month_name, num_days, year, month)
+        dataFetcher(month_name, num_days, year, month, locationTag)
     
-
-
-def testHistory():
-    print("hello from history")
 
 if __name__ == "__main__":
     main()
